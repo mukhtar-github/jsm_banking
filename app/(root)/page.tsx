85d23355bfc460f977a9325ec0f4aaa6b401a772
Mukhtartg <mukhtartanimugarba@gmail.com>
2024-05-11 15:40:37 +0100

    implement HeaderBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {HeaderBox, TotalBalanceBox} from '@/components/HeaderBox'
+
+const loggedIn = { firstName: 'Mukhtar'};
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account transaction efficiently'
+          />
+
+          <TotalBalanceBox />
+        </header>
+      </div>
+    </section>
   )
 }
 
