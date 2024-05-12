288102094149d101c2fc4e31b035547d7300256c
Mukhtartg <mukhtartanimugarba@gmail.com>
2024-05-12 07:14:11 +0100

    implement changes

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -1,11 +1,17 @@
-'use client';
+'use client'
 
 import CountUp from "react-countup"
 
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <div>
-        <CountUp end={amount} />
+    <div className="w-full">
+      <CountUp
+        duration={2}
+        decimals={2}
+        decimal="."
+        prefix="$"
+        end={amount} 
+      />
     </div>
   )
 }
---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { Doughnut } from 'react-chartjs-2';
+
+const DoughnutChart = () => {
+  return <Doughnut data={[]} />
+}
+
+export default DoughnutChart
